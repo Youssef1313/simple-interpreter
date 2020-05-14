11752099bc69059d92c5e7fd41725a915a47cb84
Youssef Victor <youssefvictor00@gmail.com>
2020-05-14 19:39:04 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+//    FileReader reader("Src.txt");
+//    Compilation compilation;
+//    string line;
+//    while (line = reader.readLine()) {
+//        Variable variable = compilation.process(line);
+//        /*
+//         * Internally, the process method will parse the instruction, evaluate the expression, and
+//         * return the variable name and its value (Variable can be a struct).
+//         */
+//        // TODO: Add the variable to BST and heap.
+//
+//    }
+    return 0;
+}
